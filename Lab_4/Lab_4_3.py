dae35f597d76766deb65f0fc1ae02131ef8d51b9
Mikhail Golikov <prototpe7212@gmail.com>
2024-12-24 20:52:43 +0300

    Update Docks

--- Lab_4/Lab_4_3.py
+++ Lab_4/Lab_4_3.py
@@ -1,13 +1,11 @@
-from Paket.work_with_digits import delete_signs_from_list as perfect_function, add
-from Paket.Work_with_strings import reverse_string
+import Paket
 
-
-print(add(2, 3)) # 5
+print(Paket.work_with_digits.add(2, 3)) # 5
 
 print('-'*100)
 
-print(perfect_function([-1, 0, 1])) # [1, 0, 1]
+print(Paket.work_with_digits.delete_signs_from_list([-1, 0, 1])) # [1, 0, 1]
 
 print('-'*100)
 
-print(reverse_string('Execution')) # noitucexE
+print(Paket.Work_with_strings.reverse_string('Execution')) # noitucexE
--- Lab_4/Paket/__init__.py
+++ Lab_4/Paket/__init__.py
@@ -1,1 +1,4 @@
-print('Paket has been importes SUCSESFULLY')
+from .work_with_digits import delete_signs_from_list, add
+from .Work_with_strings import reverse_string
+
+__all__ = ['add', 'delete_signs_from_list', 'reverse_string']
